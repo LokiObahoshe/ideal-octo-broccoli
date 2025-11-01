96c8fc5cd34f043ad3b6b593090b4912e3b047d8
LokiObahoshe <sondereggerbecca@gmail.com>
2025-11-01 11:21:36 -0500

    Added Dynamic Array Functions

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,24 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
+
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Create an array for the results using double so that it can hold decimals and give it a length
+        double[] result = new double[length];
+
+        // Use a loop that will repeatingly display the results
+        for (int i = 0; i < length; i++)
+        {
+            // Shift the index up using a number (or do the math, in otherwords). For example, 3(<--number) * (1(<--index)+1) = 6
+            result[i] = number * (i + 1);
+        }
+
+        // Use a return for results
+        return result;
     }
 
     /// <summary>
@@ -29,5 +41,20 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Check if the list is empty. If not, then don't continue (use a return)
+        if (data == null || data.Count == 0)
+        {
+            return;
+        }
+
+        // Create the list, but seperate it into two lists so that the rotation is possible
+        List<int> suffixPart = data.GetRange(data.Count - amount, amount);
+        List<int> prefixPart = data.GetRange(0, data.Count - amount);
+
+        // Clear the lists and rebuild them
+        data.Clear();
+        data.AddRange(suffixPart);
+        data.AddRange(prefixPart);
     }
 }
